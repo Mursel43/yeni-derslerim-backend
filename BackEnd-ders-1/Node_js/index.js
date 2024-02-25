8338cd52b19d6406f908fe7eb3027dd313ed46b7
Mursel43 <yusiflimursel43@gmail.com>
2024-02-26 00:28:18 +0400

    Update 1.0.1

---
+++ BackEnd-ders-1/Node_js/index.js
@@ -0,0 +1,9 @@
+// for (let i=0; i<100; i++){
+//     console.log("Hello World!",Math.random());
+// }
+
+ const {add,mul} =require ("./kitabxanam");
+const axios =require("axios");
+
+// var cem=add(7,2);
+// console.log(cem);
